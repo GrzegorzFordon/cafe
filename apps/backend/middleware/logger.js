17668664511cc7f1108b507dc8ae6f6417d33697
GrzegorzFordon <Grzegorz.fordon@gmail.com>
2026-05-27 17:04:16 +0200

    logging taken out temporarily

--- apps/backend/middleware/logger.js
+++ apps/backend/middleware/logger.js
@@ -6,22 +6,22 @@ import path from "path";
 const __dirname = import.meta.dirname;
 
 const logEvents = async (message, logFileName) => {
-  const dateTime = format(new Date(), "yyyyMMdd\tHH:mm:ss");
-  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
-  try {
-    const logsPath = path.join(__dirname, "..", "logs");
-    if (!fs.existsSync(logsPath)) {
-      await fsPromises.mkdir(logsPath);
-    }
-    await fsPromises.appendFile(path.join(logsPath, logFileName), logItem);
-  } catch (error) {
-    console.log(error);
-  }
+  // const dateTime = format(new Date(), "yyyyMMdd\tHH:mm:ss");
+  // const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+  // try {
+  //   const logsPath = path.join(__dirname, "..", "logs");
+  //   if (!fs.existsSync(logsPath)) {
+  //     await fsPromises.mkdir(logsPath);
+  //   }
+  //   await fsPromises.appendFile(path.join(logsPath, logFileName), logItem);
+  // } catch (error) {
+  //   console.log(error);
+  // }
 };
 
 const logger = (req, res, next) => {
-  logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
-  console.log(`${req.method} ${req.path}`);
+  // logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
+  // console.log(`${req.method} ${req.path}`);
   next();
 };
 
