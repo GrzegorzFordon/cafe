b5befce7771cc91aad15c0a0cf241be9eb5c4b29
GrzegorzFordon <Grzegorz.fordon@gmail.com>
2026-05-27 13:30:41 +0200

    cors allowed origins

--- apps/backend/config/allowedOrigins.js
+++ apps/backend/config/allowedOrigins.js
@@ -1,3 +1,8 @@
-const allowedOrigins = ["http://localhost:3000","http://localhost:5173","http://192.168.2.115:5173","*"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "http://192.168.2.115:5173",
+  "https://cafe-frontend-cyan.vercel.app",
+];
 
 export default allowedOrigins;
