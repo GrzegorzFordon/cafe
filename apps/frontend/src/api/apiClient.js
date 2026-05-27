63f25278421bf146de93a83262e4b03b02ad6889
GrzegorzFordon <Grzegorz.fordon@gmail.com>
2026-05-27 15:25:36 +0200

    frontend bugs

--- apps/frontend/src/api/apiClient.js
+++ apps/frontend/src/api/apiClient.js
@@ -1,12 +1,13 @@
 // AXIOS CLIENT INSTANCE
 // set the base url and common things
 // services will dictate what method and what endpoint they wanna use
+import "dotenv/config";
 import axios from "axios";
-import process from "dotenv/config";
-// const BACKEND_URL = "http://localhost:3500";
+
+const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3500";
 
 const apiClient = axios.create({
-  baseURL: process.env.BACKEND_URL,
+  baseURL: BACKEND_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
