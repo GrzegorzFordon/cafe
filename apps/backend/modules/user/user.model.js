f5503f712ef3952eed1263709ec2aebbda8d8909
GrzegorzFordon <Grzegorz.fordon@gmail.com>
2026-05-28 16:31:04 +0200

    mongoose

--- apps/backend/modules/user/user.model.js
+++ apps/backend/modules/user/user.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import getInstance from "../../config/dbConn.js";
 
-// const mongooseInstance = getInstance();
+const mongooseInstance = getInstance();
 const userSchema = new mongoose.Schema(
   {
     name: {
