9c7c8de3fd3d445fc693657216a0fdf0d4beea91
GrzegorzFordon <Grzegorz.fordon@gmail.com>
2026-05-29 13:40:21 +0200

    image import 02

--- apps/frontend/src/features/play/game/card/CardVisual.jsx
+++ apps/frontend/src/features/play/game/card/CardVisual.jsx
@@ -1,20 +1,20 @@
 // import * as VFX from "react-vfx";
 // eslint-disable-next-line no-unused-vars
 import { AnimatePresence, easeOut, motion, Reorder } from "motion/react";
-import { useMemo } from "react";
+// import { useMemo } from "react";
 // import useCardList from "../hooks/useCardList";
 function CardVisual({ card, order }) {
   // const sprite = useCardList(card?.cardID);
-  const spritePath = useMemo(
-    () => `../assets/cards/${card?.cardID ?? "def"}.png`,
-    [card.cardID],
-  );
+  const spritePath = async () => {
+    const path = `../assets/cards/${card?.cardID ?? "def"}.png`;
+    return await import(path);
+  };
 
   return (
     <motion.img
       key={order}
       draggable="false"
-      src={import(spritePath)}
+      src={spritePath}
       alt={card?.cardID}
       className="size-fit object-scale-down select-none"
     />
